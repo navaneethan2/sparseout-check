327780d5a18067443d1096b5d4cf3fcb7fd21c70
navaneethan2 <naveenhari55@gmail.com>
2021-08-06 20:18:51 +0530

    0003 - Updated Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -15,7 +15,7 @@
          choice(name: 'env', choices: ['ci', 'Dev'], description: 'To load the parameters in the jenkinfile.')
          choice(name: 'App_Module_Name', choices: ['Module_1', 'Module_2', 'Module_3', 'Module_4',], description: 'The number of parallel customer and payment regression packs to run.')
          booleanParam(name: 'Send_Email', description: 'Send Email',  defaultValue: true)
-         text(name: 'Email_To',defaultValue: 'navaneethan.chinnasamy@rci.com',description: 'Email recipients')
+         text(name: 'Email_To',defaultValue: 'navaneethan.chinnasamy@gmail.com',description: 'Email recipients')
      }
 
 
