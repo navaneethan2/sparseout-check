03536f2370c2708ce7a721be39d272f4e2ae39a4
Navaneethan <30861038+navaneethan2@users.noreply.github.com>
2021-09-07 13:03:05 +0530

    Update Jenkinsfile1.groovy

--- Jenkinsfile1.groovy
+++ Jenkinsfile1.groovy
@@ -1,1 +1,17 @@
+pipeline{
+      agent any
+  
+  stages {
+    stage(''check change) {
+      when {
+         changeset "sparseout-check"
+      }
+      steps{
+          sh "echo I will build now"
+      }
+    
+    }
+  
+  }
 
+}
